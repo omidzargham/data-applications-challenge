72f706976b3a1c288c5673bad20ef7724af9f599
Omid Zargham <ozargham@berkeley.edu>
2018-03-22 13:58:32 -0700

    Rendered stations on map from API

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-var mymap = L.map('map').setView([39.8283, -98.5795], 4);
+var mymap = L.map('map').setView([39.8283, -98.5795], 5);
 
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(mymap);
+const mapBoxToken = "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapBoxToken, {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'mapbox.streets'
+}).addTo(mymap);
+
+var baseurl = 'https://api.voltaapi.com/v1/'
+var greenOrRed = "green";
+
+var theData = fetch(baseurl + 'stations')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    for (var i=0; i < myJson.length; i++) {
+      if (myJson[i].status === "active") {
+        greenOrRed = "green";
+      } else if (myJson[i].status === "needs service") {
+          greenOrRed = "red";
+        }
+        
+        L.circleMarker([myJson[i].location.coordinates[1], myJson[i].location.coordinates[0]], {
+          color: greenOrRed,
+          fillColor: greenOrRed,
+          fillOpacity: .3,
+          radius: 5
+      }).addTo(mymap);
+    }
+  });

b3d26c9f6bcac75cfd3d0d695d7e0285bf373eb9
Omid Zargham <ozargham@berkeley.edu>
2018-03-22 18:31:56 -0700

    added legend and performed refactorings

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var mymap = L.map('map').setView([39.8283, -98.5795], 5);
+var myMap = L.map('map').setView([39.8283, -98.5795], 5);
 
 const mapBoxToken = "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
 
@@ -8,30 +8,46 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
   'Imagery © <a href="http://mapbox.com">Mapbox</a>',
   id: 'mapbox.streets'
-}).addTo(mymap);
+}).addTo(myMap);
 
-var baseurl = 'https://api.voltaapi.com/v1/'
-var greenOrRed = "green";
+const baseurl = 'https://api.voltaapi.com/v1/'
+var greenOrRed;
 
 var theData = fetch(baseurl + 'stations')
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
-    console.log(myJson)
-    for (var i=0; i < myJson.length; i++) {
-      if (myJson[i].status === "active") {
+    for (var i = 0; i < myJson.length; i++) {
+      currentElem = myJson[i];
+      if (currentElem.status === "active") {
         greenOrRed = "green";
-      } else if (myJson[i].status === "needs service") {
+      } else if (currentElem.status === "needs service") {
           greenOrRed = "red";
         }
         
-        L.circleMarker([myJson[i].location.coordinates[1], myJson[i].location.coordinates[0]], {
+        L.circleMarker([currentElem.location.coordinates[1], currentElem.location.coordinates[0]], {
           color: greenOrRed,
           fillColor: greenOrRed,
           fillOpacity: .3,
           radius: 6
-        }).addTo(mymap).bindPopup("<center>" + myJson[i].name 
-        + "<br>" + "Station ID: " + myJson[i].id + "</center>");
+        }).addTo(myMap).bindPopup("<center>" + currentElem.name 
+        + "<br>" + "Station ID: " + currentElem.id + "</center>");
     }
   });
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function() {
+  var div = L.DomUtil.create('div', 'info legend');
+  var keyName = ["Active", "Needs Service"];
+  var labels = ["green", "red"];
+
+  for (var i = 0; i < keyName.length; i++) {
+    div.innerHTML += '<i style="background:' + labels[i] + '"></i> ' + keyName[i] + '<br>';
+  }
+
+  return div;
+};
+
+legend.addTo(myMap);
